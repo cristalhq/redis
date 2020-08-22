9f16f16fa80c0998fa20cd2b877403286c9cf5ee
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-08-22 12:18:48 +0200

    update initial Set

--- set.go
+++ set.go
@@ -4,95 +4,85 @@ import "github.com/go-redis/redis"
 
 // Set represents a Redis Set structure.
 type Set struct {
+	name   string
 	client *redis.Client
 }
 
-// SAdd add one or more members to a set.
-func (s *Set) SAdd(key string, members ...interface{}) (int64, error) {
-	resp := s.client.SAdd(key, members)
-	return resp.Result()
+func NewSet(name string, client *redis.Client) *Set {
+	return &Set{name: name, client: client}
 }
 
-// SCard get the number of members in a set.
-func (s *Set) SCard(key string) (int64, error) {
-	resp := s.client.SCard(key)
-	return resp.Result()
+// Add add one or more members to a set.
+func (s *Set) Add(members ...interface{}) (int64, error) {
+	return s.client.SAdd(s.name, members).Result()
 }
 
-// SDiff subtract multiple sets.
-func (s *Set) SDiff(keys ...string) ([]string, error) {
-	resp := s.client.SDiff(keys...)
-	return resp.Result()
+// Cardinality get the number of members in a set.
+func (s *Set) Cardinality(key string) (int64, error) {
+	return s.client.SCard(key).Result()
 }
 
-// SDIFFSTORE subtract multiple sets and store the resulting set in a key.
-func (s *Set) SDIFFSTORE(dst string, keys ...string) (int64, error) {
-	resp := s.client.SDiffStore(dst, keys...)
-	return resp.Result()
+// IsMember determine if a given value is a member of a set.
+func (s *Set) IsMember(member interface{}) (bool, error) {
+	return s.client.SIsMember(s.name, member).Result()
 }
 
-// SInter intersect multiple sets.
-func (s *Set) SInter(keys ...string) ([]string, error) {
-	resp := s.client.SInter(keys...)
-	return resp.Result()
+// Members get all the members in a set.
+func (s *Set) Members(key string) ([]string, error) {
+	return s.client.SMembers(key).Result()
 }
 
-// SInterstore intersect multiple sets and store the resulting set in a key.
-func (s *Set) SInterstore(dst string, keys ...string) (int64, error) {
-	resp := s.client.SInterStore(dst, keys...)
-	return resp.Result()
+// Move move a member from one set to another.
+func (s *Set) Move(src, dst string, member interface{}) (bool, error) {
+	return s.client.SMove(src, dst, member).Result()
 }
 
-// SIsMember determine if a given value is a member of a set.
-func (s *Set) SIsMember(key string, member interface{}) (bool, error) {
-	resp := s.client.SIsMember(key, member)
-	return resp.Result()
+// Pop remove and return one or multiple random members from a set.
+func (s *Set) Pop(key string) (string, error) {
+	return s.client.SPop(key).Result()
 }
 
-// SMembers get all the members in a set.
-func (s *Set) SMembers(key string) ([]string, error) {
-	resp := s.client.SMembers(key)
-	return resp.Result()
+// RandomMember get one or multiple random members from a set.
+func (s *Set) RandomMember(key string) (string, error) {
+	return s.client.SRandMember(key).Result()
 }
 
-// SMove move a member from one set to another.
-func (s *Set) SMove(src, dst string, member interface{}) (bool, error) {
-	resp := s.client.SMove(src, dst, member)
-	return resp.Result()
+// Remove Remove one or more members from a set
+func (s *Set) Remove(members ...interface{}) (int64, error) {
+	return s.client.SRem(s.name, members).Result()
 }
 
-// SPop remove and return one or multiple random members from a set.
-func (s *Set) SPop(key string) (string, error) {
-	resp := s.client.SPop(key)
-	return resp.Result()
+// Scan incrementally iterate Set elements.
+func (s *Set) Scan(cursor uint64, match string, count int64) ([]string, uint64, error) {
+	return s.client.SScan(s.name, cursor, match, count).Result()
 }
 
-// SRandMember get one or multiple random members from a set.
-func (s *Set) SRandMember(key string) (string, error) {
-	resp := s.client.SRandMember(key)
-	return resp.Result()
+// Difference subtract multiple sets.
+func (s *Set) Difference(keys ...string) ([]string, error) {
+	return s.client.SDiff(keys...).Result()
 }
 
-// SRem Remove one or more members from a set
-func (s *Set) SRem(key string, members ...interface{}) (int64, error) {
-	resp := s.client.SRem(key, members)
-	return resp.Result()
+// DIFFSTORE subtract multiple sets and store the resulting set in a key.
+func (s *Set) DIFFSTORE(dst string, keys ...string) (int64, error) {
+	return s.client.SDiffStore(dst, keys...).Result()
 }
 
-// SScan incrementally iterate Set elements.
-func (s *Set) SScan(key string, cursor uint64, match string, count int64) ([]string, uint64, error) {
-	resp := s.client.SScan(key, cursor, match, count)
-	return resp.Result()
+// Intersection intersect multiple sets.
+func (s *Set) Intersection(keys ...string) ([]string, error) {
+	return s.client.SInter(keys...).Result()
 }
 
-// SUnion add multiple sets.s
-func (s *Set) SUnion(keys ...string) ([]string, error) {
-	resp := s.client.SUnion(keys...)
-	return resp.Result()
+// Interstore intersect multiple sets and store the resulting set in a key.
+func (s *Set) Interstore(dst string, keys ...string) (int64, error) {
+	return s.client.SInterStore(dst, keys...).Result()
 }
 
-// SUnionStore add multiple sets and store the resulting set in a key.
-func (s *Set) SUnionStore(dst string, keys ...string) (int64, error) {
-	resp := s.client.SUnionStore(dst, keys...)
-	return resp.Result()
+// Union add multiple sets.
+func (s *Set) Union(keys ...string) ([]string, error) {
+	return s.client.SUnion(keys...).Result()
+}
+
+// UnionStore add multiple sets and store the resulting set in a key.
+func (s *Set) UnionStore(dst string, keys ...string) (int64, error) {
+	return s.client.SUnionStore(dst, keys...).Result()
 }
