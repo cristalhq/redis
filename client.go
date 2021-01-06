6e7f9c0e925c2b886314eb44da75e7e437998917
Oleg Kovalov <oleg@hey.com>
2021-01-06 13:53:45 +0100

    update client

--- client.go
+++ client.go
@@ -19,9 +19,34 @@ func NewClient(client *redisClient) (*Client, error) {
 	return c, nil
 }
 
+// BitMap returns a client for Redis BitMap structure.
+func (c *Client) BitMap(name string) *BitMap {
+	return NewBitMap(name, c.client)
+}
+
 // Geo returns a client for Redis Geo structure.
 func (c *Client) Geo(name string) *Geo {
 	return NewGeo(name, c.client)
+}
+
+// HashMap returns a client for Redis HashMap structure.
+func (c *Client) HashMap(name string) *HashMap {
+	return NewHashMap(name, c.client)
+}
+
+// HyperLogLog returns a client for Redis HyperLogLog structure.
+func (c *Client) HyperLogLog(name string) *HyperLogLog {
+	return NewHyperLogLog(name, c.client)
+}
+
+// List returns a client for Redis List structure.
+func (c *Client) List(name string) *List {
+	return NewList(name, c.client)
+}
+
+// SortedSet returns a client for Redis SortedSet structure.
+func (c *Client) SortedSet(name string) *SortedSet {
+	return NewSortedSet(name, c.client)
 }
 
 // Set returns a client for Redis Set structure.
@@ -29,7 +54,7 @@ func (c *Client) Set(name string) *Set {
 	return NewSet(name, c.client)
 }
 
-// HyperLogLog returns a client for Redis HyperLogLog structure.
-func (c *Client) HyperLogLog(name string) *HyperLogLog {
-	return NewHyperLogLog(name, c.client)
+// Stream returns a client for Redis Stream structure.
+func (c *Client) Stream(name string) *Stream {
+	return NewStream(name, c.client)
 }
